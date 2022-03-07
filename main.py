244048ba737325f9d1c684247da8a30fe03c20e6
Damian Jeździkowski <dejmienblue@gmail.com>
2022-03-08 00:56:59 +0100

    TODO list added

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ while True:
     addItem.addItem()
     
     answer = input('Do you want to add another item? [Y/N]: ').lower()
-    if answer == 'n':
-        break
+    if answer == 'y':
+        continue
+    else: break
 
 db.con.close()
 exit()
+
+# TODO: 
+    # create database infrastructure - adequate to the intentions   <=  dzikitatus  done.
+    # create database library                                       <=  Trotylson
+    # create 'addItem' function                                     <=  Trotylson   done.
+    # create 'editItem' function                                    <=  dzikitatus
+    # create 'deleteItem' function                                  <=  dzikitatus
